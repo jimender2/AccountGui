bc7b4c26f4c0c625681584475e1bb37dd1886a81
Jonathan Meredith <35303639+jimender2@users.noreply.github.com>
2019-02-16 14:53:51 -0500

    Edit

--- powershell.ps1
+++ powershell.ps1
@@ -4,8 +4,8 @@ $lname=$args[2]
 $pword=$args[3]
 $org=$args[4]
 $changePass=$args[5]
-$creatorusername = "blrv\blrv"
-$creatorpassword = ConvertTo-SecureString "blrv" -AsPlainText -Force
+$creatorusername = ""
+$creatorpassword = ConvertTo-SecureString "" -AsPlainText -Force
 $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $creatorusername, $creatorpassword
 $secpasswd = ConvertTo-SecureString -String $pword -AsPlainText -Force
-New-ADuser -Name $name -GivenName $fname -Surname $lname -DisplayName $name -AccountPassword $secpasswd -Organization $org -Server blrv3.blackriver.k12.oh.us -CannotChangePassword $changePass -Credential $cred
+New-ADuser -Name $name -GivenName $fname -Surname $lname -DisplayName $name -AccountPassword $secpasswd -Organization $org -Server  -CannotChangePassword $changePass -Credential $cred
